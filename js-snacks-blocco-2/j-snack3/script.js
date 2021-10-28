d60f1e5ac1954d36312b431bd90bdbc8e2bcef61
giulio30795 <giulioincarbone95@gmail.com>
2021-10-28 18:47:41 +0200

    done

---
+++ js-snacks-blocco-2/j-snack3/script.js
@@ -0,0 +1,8 @@
+const minore = ['1','2','3','4']
+const maggiore = ['1','2','3','4','5','6']
+
+while (minore.length != maggiore.length){
+    minore.push(Math.floor(Math.random() * 5)
+    )
+    console.log(minore, maggiore);
+}
