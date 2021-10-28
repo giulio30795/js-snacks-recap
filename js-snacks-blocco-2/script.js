1b26bbbecffd9f54556e70e847f5ebe2e20881a3
giulio30795 <giulioincarbone95@gmail.com>
2021-10-28 15:35:19 +0200

    jsnackblocco2-1

---
+++ js-snacks-blocco-2/script.js
@@ -0,0 +1,12 @@
+// Inserisci un numero, se è pari stampa il numero,
+// se è dispari stampa il numero successivo
+console.log('wow');
+
+const numero = parseInt( (prompt('inserisci un numero')) )
+
+if (numero % 2 != 0 ){
+    
+    console.log(numero + 1);
+} else {
+    console.log(numero);
+}
