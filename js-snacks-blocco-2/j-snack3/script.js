a19a24499d7f198c1f898eeca207bb3f664b2036
giulio30795 <giulioincarbone95@gmail.com>
2021-10-28 18:48:28 +0200

    check

--- js-snacks-blocco-2/j-snack3/script.js
+++ js-snacks-blocco-2/j-snack3/script.js
@@ -1,3 +1,8 @@
+// Crea due array che hanno un numero di elementi diversi.
+// Aggiungi elementi casuali all’array che ha meno elementi,
+// fino a quando ne avrà tanti quanti l’altro.
+
+
 const minore = ['1','2','3','4']
 const maggiore = ['1','2','3','4','5','6']
 
